7ceaa55937dc7794d9f3c7b42e10aa13f2afe5f6
Jeff Doyle <shelleydoyle@eastlink.ca>
2018-03-17 15:56:00 -0300

    make minimum.fs work with latest if.atmega

--- words/minimum.fs
+++ words/minimum.fs
@@ -4,7 +4,7 @@
 ( n min max -- f)
 \ check if n is within min..max
 : within
-    over - >a - a u<
+    pop.y -y !x d0 -y !d0 x u<
 ;
 
 \ increment a cvar by one.  If val > max then set flag to true.
@@ -18,12 +18,11 @@
 : edefer
     (create)
     cur@ !e
-    compile (def)
+    program: (defe)
 
     edp             ( -- EDP )
     push            ( -- EDP EDP )
     ,               ( -- EDP )
-    ['] @e ,
     ['] !e ,
     \ increment EDP one cell then save it
     dcell+          ( -- EDP+2 )
@@ -37,12 +36,11 @@
     (create)
     cur@ !e
 
-    compile (def)
+    program: (defr)
 
     here ,
     dcell allot
 
-    ['] @ ,
     ['] ! ,
 ;
 
@@ -50,9 +48,9 @@
 \ signed multiply n1 * n2 and division  with n3 with double
 \ precision intermediate and remainder
 : */mod  ( n1 n2 n3 -- rem quot )
-    >r
+    rpush pop
     m*
-    r>
+    push rpop
     um/mod
 ;
 
@@ -101,13 +99,14 @@
 \ only accepts positive values
 : spaces ( n -- )
     \ make sure a positive number
-    push 0> and
+    !y  0> and.y ( n' Y:n )
+    push         ( n' n' )
     begin
     ?while
       space
-      1- 
+      d0 1- !d0
     repeat
-    pop
+    nip
 ;
 
 \ pointer to current write position
@@ -117,27 +116,27 @@ var hld
 
 \ prepend character to pictured numeric output buffer
 : hold ( c -- )
-    -1 hld +!   
-    hld @ c!
+    !y hld 1-!   
+    @ y.c!
 ;
 
 \ Address of the temporary scratch buffer.
 : pad ( -- a-addr )
-    here 20 +
+    y= 20 here +y
 ;
 
 \ initialize the pictured numeric output conversion process
-: <# ( -- )
-    pad hld !
+: <# ( n -- n )
+    push pad !y hld y.! pop
 ;
 
 
 \ pictured numeric output: convert one digit
 : # ( u1 -- u2 )
-    base@      ( u1 base )
-    u/mod      ( rem u2 )
-    swap       ( u2 rem )
-    #h hold    ( u2 )
+    push base@  ( u1 base )
+    u/mod       ( rem u2 )
+    swap        ( u2 rem )
+    #h hold pop ( u2 )
 ;
 
 \ pictured numeric output: convert all digits until 0 (zero) is reached
@@ -152,35 +151,34 @@ var hld
 
 \ Pictured Numeric Output: convert PNO buffer into an string
 : #> ( u1 -- addr count )
-    pop hld @ pad over -
+    hld @ !x    ( addr Y: addr )
+    push pad -x ( addr pad-addr )
 ;
 
 \ place a - in HLD if n is negative
 : sign ( n -- )
-    0< if [char] - hold then
+    0< ?if [char] - hold then
 ;
 
 
 \ singed PNO with cell numbers, right aligned in width w
 : .r ( wantsign n w -- )
-    >r   ( wantsign n ) ( R: w )
-    <#
-    #s   ( wantsign 0 )
-    swap ( 0 wantsign )
-    sign ( 0 )
-    #>   ( addr len )
-    r>   ( addr len w )  ( R: )
-    over ( addr len w len )
-    -    ( addr len spaces )
-    spaces ( addr len )
-    type  ( )
+    rpush pop  ( wantsign n ) ( R: w )
+    <# #s      ( wantsign 0 )
+    pop sign   ( ? )
+    #>         ( addr len )
+    push rpop  ( addr len w )  ( R: )
+    d0!y       ( addr len w Y:len )
+    -y         ( addr len spaces )
+    spaces     ( addr len ? )
+    pop type   ( )
     space
 ;
 
 \ unsigned PNO with single cell numbers
 : u. ( u -- )
-    0      ( n 0 ) \ want unsigned
-    tuck   ( 0 n 0 )
+    push2 0 ( u u 0 ) \ want unsigned
+    !d1     ( 0 u 0 )
     .r 
 ;
 
@@ -188,42 +186,28 @@ var hld
 \ singed PNO with single cell numbers
 : .  ( n -- )
     push      ( n n )
-    abs      ( n n' )
-    0        ( n n' 0 ) \ not right aligned
+    abs       ( n n' )
+    push 0    ( n n' 0 ) \ not right aligned
     .r
 ;
 
 \ stack dump
 : .s  ( -- ) 
-    sp@     ( limit ) \ setup limit
-    dcell- 
-    sp0     ( limit counter )
-    begin 
-    dcell-  ( limit counter-2 )
-    2over   ( limit counter-2 limit counter-2 )
-    <>      ( limit counter-2 flag )
-    while 
-      push    ( limit counter-2 counter-2 )
-      @       ( limit counter-2 val )
-      u.      ( limit counter-2 )
-    repeat   
-    pop2
+    push        ( ?  ? )
+    sp          ( ? limit ) \ setup limit
+    dcell-
+    push sp0    ( ? limit counter )
+    begin
+      dcell-    ( ? limit counter-2 )
+      2over     ( ? limit counter-2 limit counter-2 )
+      <>        ( ? limit counter-2 flag )
+      ?while
+        d0      ( ? limit counter-2 counter-2 )
+        @       ( ? limit counter-2 val )
+        . pop   ( ? limit counter-2 )
+    repeat
+    nip2 pop
 ;
-
-\ numbers that get used a lot
-
-: 3 3 ;
-: 4 4 ;
-: 5 5 ;
-: 6 6 ;
-: 7 7 ;
-: 8 8 ;
-: 9 9 ;
-: $FF $FF ;
-: $F0 $F0 ;
-: $0F $0F ;
-: $FF00 $FF00 ;
-
 
 ( xt1 c<char> -- ) 
 \ stores xt into defer or compiles code to do so at runtime
@@ -236,12 +220,12 @@ var hld
 \ n is the address of the table entry
 \ only need to write the address 
 \ jmp instruction is already in vector table
-: isr 1+ ' swap !i ;
+: isr 1+ push ' swap !i ;
 
 ( C: name -- )
 \ start defining an Interrupt Service Routine
-: :isr : compile (i:) ; immediate
+: :isr : program: (i:) ; immediate
 
 ( -- )
 \ finish defining an Interrupt Service Routine
-: ;isr compile (i;) [compile] ; ; :ic
+: ;isr program: (i;) [compile] ; ; :ic
